6011b56726f80b489ebf79fc248ff104a866a460
Jordan Rodriguez <jordan_r_a@hotmail.es>
2022-05-29 10:14:12 -0500

    Suben cambios de turbolinks en reporte de vendata

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -154,7 +154,7 @@
                      </a>
                      <ul class="full-width menu-principal sub-menu-options">
                          <li class="full-width">
-                             <a href="sales.report" class="full-width" data-turbolinks="false">
+                             <a href="sales.report" class="full-width">
                                  <div class="navLateral-body-cl">
                                      <i class="zmdi zmdi-view-list-alt"></i>
                                  </div>
@@ -164,7 +164,7 @@
                              </a>
                          </li>
                          <li class="full-width">
-                             <a href="sale" class="full-width">
+                             <a href="sale" class="full-width" data-turbolinks="false">
                                  <div class="navLateral-body-cl">
                                      <i class="zmdi zmdi-shopping-cart-add"></i>
                                  </div>
